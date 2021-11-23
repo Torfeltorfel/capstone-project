8c2078bd41ef8c964781a0f3908b67959fa8f5bc
Torben Feldmann <torben.feldmann1@gmail.com>
2021-11-23 18:24:03 +0100

    Add Routes for MeditationPlayer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MeditationPlayer from './pages/meditationPlayer/MeditationPlayer';
 
 function App(): JSX.Element {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<MeditationPlayer>Click</MeditationPlayer>}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
