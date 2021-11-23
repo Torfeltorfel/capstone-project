e7a6ddc443fb6f14e405aa294555ef14f5118148
Torben Feldmann <torben.feldmann1@gmail.com>
2021-11-23 18:24:58 +0100

    Add simple button with gong-sound

--- src/app/pages/meditationPlayer/MeditationPlayer.tsx
+++ src/app/pages/meditationPlayer/MeditationPlayer.tsx
@@ -1,6 +1,9 @@
 import type { ReactNode } from 'react';
+import { useState } from 'react';
 import React from 'react';
 import styled from 'styled-components';
+import useSound from 'use-sound';
+import gongSound from './assets/gong-sound.wav';
 
 type MeditationPlayerProps = {
   children: ReactNode;
@@ -9,10 +12,19 @@ type MeditationPlayerProps = {
 export default function MeditationPlayer({
   children,
 }: MeditationPlayerProps): JSX.Element {
+  const [play] = useSound(gongSound);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function togglePlayPause() {
+    setIsPlaying(!isPlaying);
+    isPlaying ? play() : '';
+  }
+
   return (
     <>
       <PageContainer>
-        <PlayButton>{children}</PlayButton>
+        <ExitButton>Exit</ExitButton>
+        <PlayButton onClick={togglePlayPause}>{children}</PlayButton>
       </PageContainer>
     </>
   );
@@ -20,11 +32,16 @@ export default function MeditationPlayer({
 
 const PageContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100%;
   width: 100%;
   background-color: yellow;
+`;
+
+const ExitButton = styled.button`
+  align-self: flex-end;
 `;
 
 const PlayButton = styled.button`
