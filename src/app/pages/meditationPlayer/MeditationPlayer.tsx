72548d73a6b7e6a1e66b39d463c0885d0abbc2b3
Torben Feldmann <torben.feldmann1@gmail.com>
2021-11-24 15:35:07 +0100

    Add countdown to audioplayer

--- src/app/pages/meditationPlayer/MeditationPlayer.tsx
+++ src/app/pages/meditationPlayer/MeditationPlayer.tsx
@@ -1,30 +1,58 @@
-import type { ReactNode } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import React from 'react';
 import styled from 'styled-components';
 import useSound from 'use-sound';
 import gongSound from './assets/gong-sound.wav';
 
 type MeditationPlayerProps = {
-  children: ReactNode;
+  hours: number;
+  minutes: number;
+  seconds: number;
 };
 
 export default function MeditationPlayer({
-  children,
+  hours,
+  minutes,
+  seconds,
 }: MeditationPlayerProps): JSX.Element {
-  const [play] = useSound(gongSound);
+  const [playGong] = useSound(gongSound);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [over, setOver] = useState(false);
+  const [[h, m, s], setTime] = useState([hours, minutes, seconds]);
 
   function togglePlayPause() {
     setIsPlaying(!isPlaying);
-    isPlaying ? play() : '';
+    isPlaying ? '' : playGong();
   }
+
+  const countdown = () => {
+    if (!isPlaying || over) return;
+    if (h === 0 && m === 0 && s === 0) setOver(true), playGong();
+    else if (m === 0 && s === 0) {
+      setTime([h - 1, 59, 59]);
+    } else if (s == 0) {
+      setTime([h, m - 1, 59]);
+    } else {
+      setTime([h, m, s - 1]);
+    }
+  };
+
+  useEffect(() => {
+    const timerID = setInterval(() => countdown(), 1000);
+    return () => clearInterval(timerID);
+  });
 
   return (
     <>
       <PageContainer>
         <ExitButton>Exit</ExitButton>
-        <PlayButton onClick={togglePlayPause}>{children}</PlayButton>
+        <PlayButton onClick={togglePlayPause}>
+          {isPlaying ? 'Pause' : 'Play'}
+        </PlayButton>
+        <p>{`${h.toString().padStart(2, '0')}:${m
+          .toString()
+          .padStart(2, '0')}:${s.toString().padStart(2, '0')}`}</p>
+        <div>{over ? "Time's up!" : ''}</div>
       </PageContainer>
     </>
   );
