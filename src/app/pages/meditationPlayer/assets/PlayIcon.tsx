bbbff323e4b37c360a25c0401aef32ce8c0bce92
Torben Feldmann <torben.feldmann1@gmail.com>
2021-11-25 09:19:01 +0100

    Fix bugs in Icon SVGs

--- src/app/pages/meditationPlayer/assets/PlayIcon.tsx
+++ src/app/pages/meditationPlayer/assets/PlayIcon.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-export default function PlayIcon(
-  props: React.SVGProps<SVGSVGElement>
-): JSX.Element {
+export default function PlayIcon(): JSX.Element {
   return (
     <svg
       width="24"
       height="24"
       xmlns="http://www.w3.org/2000/svg"
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
     >
-      <path d="M23 12l-22 12v-24l22 12zm-21 10.315l18.912-10.315-18.912-10.315v20.63z" />
+      <path
+        fill="#FFFFFF"
+        stroke="#FFFFFF"
+        strokeWidth="1"
+        d="M23 12l-22 12v-24l22 12zm-21 10.315l18.912-10.315-18.912-10.315v20.63z"
+      />
     </svg>
   );
 }
