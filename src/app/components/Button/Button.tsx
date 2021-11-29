d4573e89e7a8392c13c964e3458466ae9fce73ee
Torben Feldmann <torben.feldmann1@gmail.com>
2021-11-29 08:33:45 +0100

    Add style to button component

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -11,9 +11,17 @@ export default function Button({ children }: ButtonProps): JSX.Element {
 
 const StyledButton = styled.button`
   border-radius: 1em;
-  background-image: var(--primary-green);
-  color: var(--white);
+  background-image: linear-gradient(
+    to top,
+    rgba(184, 197, 172, 1.5),
+    rgba(158, 169, 151, 1)
+  );
+  color: #fff;
+  box-shadow: 0 4px 6px hsla(0, 0%, 0%, 0.2);
   padding: 0.8em 2em;
   border: none;
   text-transform: uppercase;
+  &:active {
+    background-image: linear-gradient(to top, #b4bab0, #7c8278);
+  }
 `;
