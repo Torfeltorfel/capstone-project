8b86e1bc69ecca087db5ac8216defcf5e16f6e4c
Torben Feldmann <torben.feldmann1@gmail.com>
2021-11-30 12:19:51 +0100

    Remove unnecessary mapping for calendar data

--- src/app/components/Calendar/CalendarView.tsx
+++ src/app/components/Calendar/CalendarView.tsx
@@ -6,44 +6,11 @@ import './styles.css';
 
 export default function CalendarView(): JSX.Element {
   const sessions = JSON.parse(localStorage.getItem('sessions') || '[]');
-  const preselectedDays2 = sessions.forEach(
-    (session: {
-      h?: number;
-      m?: number;
-      day: number;
-      month: number;
-      year: number;
-    }) => {
-      delete session.h;
-      delete session.m;
-      console.log(session);
-    }
-  );
-  console.log(preselectedDays2);
-
-  const preselectedDays = [
-    {
-      day: 2,
-      month: 11,
-      year: 2021,
-    },
-    {
-      day: 4,
-      month: 11,
-      year: 2021,
-    },
-    {
-      year: 2021,
-      month: 10,
-      day: 30,
-    },
-  ];
-
   return (
     <Container>
       <Header>Your Meditation sessions</Header>
       <Calendar
-        value={preselectedDays2}
+        value={sessions}
         colorPrimary="var(--green-background)"
         calendarClassName="customCalendar"
       />
