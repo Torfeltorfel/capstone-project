8eb72cdf09edbdddf69ba29ad8a97140beaead38
Torben Feldmann <torben.feldmann1@gmail.com>
2021-12-01 18:55:33 +0100

    Get duration for player from storage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,17 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import MeditationPlayer from './pages/meditationPlayer/MeditationPlayer';
 
 function App(): JSX.Element {
+  const lastDuration = JSON.parse(localStorage.getItem('Duration') || '[]');
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/settimer" element={<SetTimer />}></Route>
         <Route
           path="/timer"
-          element={<MeditationPlayer hours={0} minutes={0} seconds={1} />}
+          element={
+            <MeditationPlayer hours={0} minutes={lastDuration} seconds={1} />
+          }
         ></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
       </Routes>
