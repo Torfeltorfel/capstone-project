ff848ecc54abb48f00d59827075f2c30394ddb27
Torben Feldmann <torben.feldmann1@gmail.com>
2021-12-02 15:18:47 +0100

    Adjust icons in navigation

--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import AnalyticsIcon from './assets/AnalyticsIcon';
+import AnalyticsIconThin from './assets/AnalyticsIconThin';
+import HomeIconThin from './assets/HomeIcon';
 import SittingIcon from './assets/SittingIcon';
 
 export default function Navigation(): JSX.Element {
   return (
     <Container>
-      <SittingIcon />
-      <AnalyticsIcon />
+      <Link to="/">
+        <HomeIconThin />
+      </Link>
+      <Link to="/settimer">
+        <SittingIcon />
+      </Link>
+      <Link to="dashboard">
+        <AnalyticsIconThin />
+      </Link>
     </Container>
   );
 }
@@ -18,6 +27,7 @@ const Container = styled.div`
   justify-content: space-around;
   height: 6rem;
   width: 100vw;
+  max-width: 25rem;
   background-color: var(--white);
   border-radius: 0.313rem 0.313rem 0 0;
   border-top: 0.031rem solid var(--grey-300);
