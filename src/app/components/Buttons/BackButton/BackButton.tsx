8d1157050c1d76f2d571f4877fce4fbea1e261ac
Torben Feldmann <torben.feldmann1@gmail.com>
2021-12-03 11:43:00 +0100

    Add centering styling to backButton

--- src/app/components/Buttons/BackButton/BackButton.tsx
+++ src/app/components/Buttons/BackButton/BackButton.tsx
@@ -11,12 +11,14 @@ export default function BackButton(): JSX.Element {
 }
 
 const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   min-height: 2rem;
   min-width: 2rem;
   border: 1px solid rgba(184, 197, 172, 1.5);
   border-radius: 50%;
   background-color: transparent;
-  align-self: flex-start;
   &:active {
     opacity: 0.5;
   }
