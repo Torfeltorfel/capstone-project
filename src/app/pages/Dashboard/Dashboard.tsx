09fc0a45c59125a2041b1d6755d5fd21e8d95eb1
Torben Feldmann <torben.feldmann1@gmail.com>
2021-12-16 18:22:45 +0100

    🔥 Remove calendar

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import Navigation from '../../components/Navigation/Navigation';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 
 export default function Dashboard(): JSX.Element {
   return (
     <Container>
-      <Calendar />
       <Navigation activeLink="dashboard" />
     </Container>
   );
